725e42e55d6f25e2d8ac8e7c07150c0b39210c9c
Zeraine <Zeraine@gmail.com>
2020-06-02 18:46:19 -0700

    Updates

---
+++ precache-manifest.3388edf8bdbca658ba545b92067a4fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1b062e8f6782974b9135600bda1884",
+    "url": "/Robofure/index.html"
+  },
+  {
+    "revision": "bc1ab60b38c681ce9e86",
+    "url": "/Robofure/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d5cc94eae6d48d622ef9",
+    "url": "/Robofure/static/css/main.db6b6ccc.chunk.css"
+  },
+  {
+    "revision": "bc1ab60b38c681ce9e86",
+    "url": "/Robofure/static/js/2.3a7abaab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofure/static/js/2.3a7abaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cc94eae6d48d622ef9",
+    "url": "/Robofure/static/js/main.4889372e.chunk.js"
+  },
+  {
+    "revision": "2e8ec51011f75397226d",
+    "url": "/Robofure/static/js/runtime-main.47434dbe.js"
+  }
+]);
